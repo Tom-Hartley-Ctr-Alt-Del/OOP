328c3c21d413adb2fc4fc2731f08875d368538e0
Tom-Hartley-Ctr-Alt-Del <a1972698@adelaide.edu.au>
2025-08-01 14:33:11 +0930

    Updated code

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,16 +1,15 @@
 bool is_fanarray(int array[], int n) {
+    bool is_fanarray=true;
     if (n<1) {
         return false;
     }
-    int mid=n/2;
-
-    for (int i=0; i<mid; i++) {
+    for (int i=0; i<n/2; i++) {
         if (array[i]!=array[n-1-i]) {
             return false;
         }
     }
     
-    for (int j=1; j<mid; j++) {
+    for (int j=1; j<n/2; j++) {
         if (array[j-1] > array[j]) {
             return false;
         }
--- main-3-1.cpp
+++ main-3-1.cpp
@@ -3,7 +3,7 @@
 extern bool is_fanarray(int[], int );
 
 int main() {
-    int array[5]={1,2,3,2,1};
-    std::cout << std::boolalpha << "The statement 'the array is a fan-array is " << is_fanarray(array, 5) << std::endl;
+    int array[5]={1,2,5,2,1};
+    std::cout << std::boolalpha << "The statement 'the array is a fan-array' is " << is_fanarray(array, 5) << std::endl;
     return 0;
 }
