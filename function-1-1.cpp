6cc213e420b0e5168217573c33f9a990addbf4a9
Tom-Hartley-Ctr-Alt-Del <a1972698@adelaide.edu.au>
2025-08-05 15:28:55 +0930

    Remove all files from the branch

--- function-1-1.cpp
+++
@@ -1,8 +0,0 @@
-int sum_diagonal(int *array) {
-    int sum=0;
-    int size=4;
-    for (int i=0; i<4; i++) {
-        sum=sum+array[i*4+i];     
-    }
-    return sum;
-}
--- main-1-3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-extern void count_digits(int *array);
-
-int main() {
-    int array[4][4]={{1,2,3,4},{1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
-    int *ptr=&array[0][0];
-    count_digits(ptr);
-    return 0;
-}
