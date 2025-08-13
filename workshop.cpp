a053ba8a626a13a22ab249539847acf88d164c11
Tom-Hartley-Ctr-Alt-Del <a1972698@adelaide.edu.au>
2025-08-13 14:33:21 +0930

    Checked that files were saved properly

--- workshop.cpp
+++ workshop.cpp
@@ -1,4 +1,33 @@
-extern void changeValue(double*);
-void printArray(double* arrayPtr, int n);
-double* dynamicArray(int size, double M);
-double arrayMax(double* arrayValues, int m);
+#include <iostream>
+#include "workshop.h"
+using namespace std;
+
+void changeValue(double* ptr) {
+    *ptr=42.0;
+    cout << *ptr << endl;
+}
+
+void printArray(double* arrayPtr, int n) {
+    for (int i=0; i<n; i++) {
+        cout << arrayPtr[i] << " ";
+    }
+    cout << endl;
+}
+
+double* dynamicArray(int size, double M) {
+    double* doubleArray= new double[size];
+    for (int j=0; j<size; j++) {
+        doubleArray[j]=M;
+    }
+    return doubleArray;
+}
+
+double arrayMax(double* arrayValues, int m) {
+    double max=arrayValues[0];
+    for(int j=1; j<m; j++) {
+        if (arrayValues[j]>max) {
+            max=arrayValues[j];
+        }
+    }
+    return max;
+}
